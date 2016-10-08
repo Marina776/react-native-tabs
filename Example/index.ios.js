e90e06c3a6ff58c0ff80d2c3540791aefcc29a36
Sharper <sharp.bruce.lee@gmail.com>
2016-10-08 21:17:39 +0800

    ES6

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -2,41 +2,40 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
 
-var React = require('react-native');
-var {
+import React from 'react-native';
+import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-} = React;
-var Tabs = require('react-native-tabs');
+} from 'react-native';
+import Tabs from 'react-native-tabs';
 
-class Example extends React.Component {
-  constructor(props){
+export default class Example extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {page:'second'};
+    this.state = { page: 'second' };
   }
   render() {
     var self = this;
     return (
       <View style={styles.container}>
-        <Tabs selected={this.state.page} style={{backgroundColor:'white'}}
-              selectedStyle={{color:'red'}} onSelect={el=>this.setState({page:el.props.name})}
-              pressOpacity={1}>
-            <Text name="first">First</Text>
-            <Text name="second" selectedIconStyle={{borderTopWidth:2,borderTopColor:'red'}}>Second</Text>
-            <Text name="third">Third</Text>
-            <Text name="fourth" selectedStyle={{color:'green'}}>Fourth</Text>
-            <Text name="fifth">Fifth</Text>
+        <Tabs selected={this.state.page} style={{ backgroundColor: 'white' }}
+          selectedStyle={{ color: 'red' }} onSelect={el => this.setState({ page: el.props.name }) }
+          pressOpacity={1}>
+          <Text name="first">First</Text>
+          <Text name="second" selectedIconStyle={{ borderTopWidth: 2, borderTopColor: 'red' }}>Second</Text>
+          <Text name="third">Third</Text>
+          <Text name="fourth" selectedStyle={{ color: 'green' }}>Fourth</Text>
+          <Text name="fifth">Fifth</Text>
         </Tabs>
-          <Text style={styles.welcome}>
-              Welcome to React Native
-          </Text>
-          <Text style={styles.instructions}>
-              Selected page: {this.state.page}
-          </Text>
+        <Text style={styles.welcome}>
+          Welcome to React Native
+        </Text>
+        <Text style={styles.instructions}>
+          Selected page: {this.state.page}
+        </Text>
       </View>
     );
   }
