f8aaee3222f9ba90b6a7ecfef66fdb48117fb876
Sharper <sharp.bruce.lee@gmail.com>
2016-10-17 21:04:07 +0800

    tab item

--- index.js
+++ index.js
@@ -1,140 +1,20 @@
-"use strict";
+/**
+ * The examples provided by Uncle Charlie are for non-commercial testing and
+ * evaluation purposes only.
+ *
+ * Uncle Charlie reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @providesModule TabCore & TabItem
+ * @flow-weak
+ */
 
-import React, {
-  Component
-} from "react";
 
-import {
-	StyleSheet,
-	View,
-	Text,
-	TouchableOpacity,
-	Keyboard,
-	Platform,
-} from "react-native";
-
-/*
- Options: ["^State$"]
-*/
-type State = {
-    keyboardUp: boolean
-};
-
-export default class Tabs extends Component {
-  state: State = {}
-
-	constructor(props: any) {
-    super(props);
-
-    this.onSelect = this.onSelect.bind(this);
-    this.keyboardWillHide = this.keyboardWillHide.bind(this);
-    this.keyboardWillShow = this.keyboardWillShow.bind(this);
-  }
-
-  onSelect(el: any){
-    if (el.props.onSelect) {
-      el.props.onSelect(el);
-    } else if (this.props.onSelect) {
-      this.props.onSelect(el);
-    }
-  }
-
-  componentWillMount(){
-    if (Platform.OS === "android") {
-      Keyboard.addListener("keyboardDidShow", this.keyboardWillShow);
-      Keyboard.addListener("keyboardDidHide", this.keyboardWillHide);
-    }
-  }
-
-  keyboardWillShow(e: any) {
-  	this.setState({ keyboardUp: true });
-  };
-
-  keyboardWillHide(e: any) {
-    this.setState({ keyboardUp: false });
-  };
-
-  render() {
-		const self = this;
-		let selected = this.props.selected;
-		if (!selected){
-			React.Children.forEach(this.props.children.filter(c=>c), (el: any): void=>{
-				if (!selected || el.props.initial){
-						selected = el.props.name || el.key;
-				}
-			});
-		}
-		return (
-			<View style={[styles.tabbarView, this.props.style, this.state.keyboardUp && styles.hidden]}>
-
-				{/* {React.Children.map(this.props.children.filter(c=>c),(el)=>
-					<TouchableOpacity key={el.props.name + "touch"}
-						testID={el.props.testID}
-						style={[styles.iconView, this.props.iconStyle, (el.props.name || el.key) == selected ? this.props.selectedIconStyle || el.props.selectedIconStyle || {} : {} ]}
-						onPress={():void=>!self.props.locked && self.onSelect(el)}
-						onLongPress={()=>self.onSelect(el)}
-						activeOpacity={el.props.pressOpacity}>
-							{selected == (el.props.name || el.key) ? React.cloneElement(el, {selected: true, style: [el.props.style, this.props.selectedStyle, el.props.selectedStyle]}) : el}
-					</TouchableOpacity> */}
-          this.props.children
-				)}
-			</View>
-		);
-  }
-}
-
-Tabs.TabItem = class TabItem {
-  state: any;
-  title: string;
-  icon: any;
-  selectedIcon: ?any;
-  selected: boolean;
-  onPress: ?() => void;
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
-      <TouchableOpacity />
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-	tabbarView: {
-		position:"absolute",
-		bottom:0,
-		right:0,
-		left:0,
-		height:50,
-		opacity:1,
-		backgroundColor:"transparent",
-		flexDirection: "row",
-		justifyContent: "center",
-		alignItems: "center",
-
-		flex: 1
-	},
-	topBorder: {
-		flexDirection: 'row',
-		position:"absolute",
-		top: 0,
-		backgroundColor: 'red',
-		height: 10,
-	},
-	iconView: {
-		flex: 1,
-		height: 40,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	hidden: {
-		height: 0,
-	},
-});
+export {TabCore as default} from './src/TabCore';
+export TabItem from './src/TabItem';
